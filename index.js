e0331aabd6a82ff547f05deb5e90b8b504b8c31e
Byron Omondi <omondibyron25@gmail.com>
2023-04-02 07:07:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  const url = 'http://localhost:3000/users';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({ name:name, email:email })
+  };
+  
+  return fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id;
+      document.body.append(id);
+    })
+    .catch(error => {
+      document.body.append(error.message);
+    });
+}
